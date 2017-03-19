06e46e3c2bec8f9b5d8f40be865dc4ff1bbbb3f9
David Malone <david@souldesign.net.nz>
2017-03-19 22:26:14 +1300

    MAJOR: Small adjustments

--- js/src/app.js
+++ js/src/app.js
@@ -2,3 +2,10 @@ window.$ = window.jQuery = require('jquery');
 global.Tether = require('tether');
 var bootstrap = require('bootstrap');
 
+
+
+$('.dropdown-chevron-mobile').on('click', function(e) {
+  $('.subcategory').toggleClass("open");
+  $('.dropdown-chevron-mobile').toggleClass("open");
+  e.preventDefault();
+});
